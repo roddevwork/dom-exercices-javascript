3628e42edd94d1f2f89c92bb33725bf04b82ff93
roddevwork <rodrigocardona87@gmail.com>
2023-05-15 23:50:26 -0300

    button_scroll.js added

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import { digitalClock, soundAlarm } from "./components/clock-alarm.js";
 import hamburgerMenu from "./components/menu_hamburger.js";
 import { moveBall } from "./components/keyboardEvents.js";
 import countDown from "./components/countdown.js";
+import scrollTopButton from "./components/button_scroll.js";
 
 document.addEventListener("DOMContentLoaded", e => {
   hamburgerMenu(".panel-btn", ".panel-aside", ".menu a");
   digitalClock("#digital-clock", "#activate-clock", "#disable-clock");
-  soundAlarm("../public/alarm_clock.ogg ", "#activate-alarm", "#disable-alarm");
+  soundAlarm("../public/sounds/alarm_clock.ogg ", "#activate-alarm", "#disable-alarm");
   countDown("countdown", "Jan 01,2024 00:00:00", "Happy new year!!");
+  scrollTopButton(".scroll-top-btn");
 });
 
 document.addEventListener("keydown", e => {
   moveBall(e, ".ball", ".stage");
 });
-
-
